081203ff2110199218b382ebdfa4b45aea1f257d
nemanjamalesija <malesija13@gmail.com>
2023-10-21 22:57:53 +0200

    Get google maps api key. Tests connection with the api

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
-function App() {
-  return <h1 className='text-red-500 uppercase'>Test</h1>;
-}
+import { useJsApiLoader, LoadScriptProps } from '@react-google-maps/api';
+import Loader from './components/Loader';
 
-export default App;
+const libs: LoadScriptProps['libraries'] = ['places'];
+
+const googleMapsKey = import.meta.env.GOOGLE_MAPS_KEY;
+
+export default function App() {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: googleMapsKey,
+    libraries: libs,
+  });
+
+  if (!isLoaded) {
+    return <Loader />;
+  }
+
+  return <h1>Success</h1>;
+}
