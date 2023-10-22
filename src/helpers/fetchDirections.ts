73c5341a3314741656e74dbec98a02f5d5ff1039
nemanjamalesija <malesija13@gmail.com>
2023-10-22 20:24:56 +0200

    Handle error in fetch directions when no route found

--- src/helpers/fetchDirections.ts
+++ src/helpers/fetchDirections.ts
@@ -11,45 +11,52 @@ export default async function fetchDirections(
   setDistance: React.Dispatch<React.SetStateAction<string>>,
   setDuration: React.Dispatch<React.SetStateAction<string>>
 ) {
-  const [originResults, destinationResults] = await Promise.all([
-    getGeocode({ address: origin }),
-    getGeocode({ address: destination }),
-  ]);
+  try {
+    const [originResults, destinationResults] = await Promise.all([
+      getGeocode({ address: origin }),
+      getGeocode({ address: destination }),
+    ]);
 
-  const [originLocation, destinationLocation] = await Promise.all([
-    getLatLng(originResults[0]),
-    getLatLng(destinationResults[0]),
-  ]);
+    const [originLocation, destinationLocation] = await Promise.all([
+      getLatLng(originResults[0]),
+      getLatLng(destinationResults[0]),
+    ]);
 
-  // Create an array of DirectionsWaypoint objects
-  const directionsWaypoints: google.maps.DirectionsWaypoint[] = waypoints.map(
-    (waypoint) => ({
-      location: waypoint,
-      stopover: true,
-    })
-  );
+    // Create an array of DirectionsWaypoint objects
+    const directionsWaypoints: google.maps.DirectionsWaypoint[] = waypoints.map(
+      (waypoint) => ({
+        location: waypoint,
+        stopover: true,
+      })
+    );
 
-  const directionsService = new google.maps.DirectionsService();
+    const directionsService = new google.maps.DirectionsService();
 
-  const results = await directionsService.route({
-    origin: originLocation,
-    destination: destinationLocation,
-    waypoints: directionsWaypoints,
-    travelMode: google.maps.TravelMode[TRAVELMODE],
-  });
+    const results = await directionsService.route({
+      origin: originLocation,
+      destination: destinationLocation,
+      waypoints: directionsWaypoints,
+      travelMode: google.maps.TravelMode[TRAVELMODE],
+    });
 
-  if (!results) return;
+    if (!results) {
+      console.log('aaa');
+      return;
+    }
 
-  const distance = results?.routes[0]?.legs[0]?.distance?.text;
-  const duration = results?.routes[0]?.legs[0]?.duration?.text;
+    const distance = results?.routes[0]?.legs[0]?.distance?.text;
+    const duration = results?.routes[0]?.legs[0]?.duration?.text;
 
-  if (!distance || !duration) return;
+    if (!distance || !duration) return;
 
-  console.log(results);
+    console.log(results);
 
-  setDirectionsResponse(results);
-  setDistance(distance);
-  setDuration(duration);
+    setDirectionsResponse(results);
+    setDistance(distance);
+    setDuration(duration);
+  } catch (error) {
+    alert(`No route could be found between ${origin} and ${destination}.`);
+  }
 
   return;
 }
