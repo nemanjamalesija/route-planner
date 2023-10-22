f654610b78a67a267c05242fdcd98a7cde1d85c3
nemanjamalesija <malesija13@gmail.com>
2023-10-22 22:34:52 +0200

    Change font family. Finish project

--- src/components/ui/Heading.tsx
+++ src/components/ui/Heading.tsx
@@ -2,7 +2,7 @@ import { HiMiniMapPin } from 'react-icons/hi2';
 
 const Heading = () => {
   return (
-    <h1 className='text-white text-2xl font-semibold text-center mb-6 flex items-center justify-center gap-2'>
+    <h1 className='text-white text-2xl font-bold text-center mb-6 flex items-center justify-center gap-2'>
       <HiMiniMapPin color='#10b981' size='32px' />
 
       <span>Route planner</span>
